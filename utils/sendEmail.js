f11da8ad18f8eaed1f643365c72d968e7983580d
iiishita05 <ishitasrivastava003@gmail.com>
2025-05-19 08:46:11 +0530

    final2

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,37 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
+let transporter;
 
-    
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
-        module.exports = async function sendEmail(to, message) {
-          await transporter.sendMail(
-            {
-                 from: process.env.EMAIL_USER,
-             to,
-                   subject: "New Notification",
-            text: message,
-          });
-                        };
+async function createTransporter() {
+  if (!transporter) {
+    const testAccount = await nodemailer.createTestAccount();
+
+    transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    });
+  }
+  return transporter;
+}
+
+async function sendEmail(to, subject, text) {
+  const transporter = await createTransporter();
+
+  const info = await transporter.sendMail({
+    from: '"Notification Service" <no-reply@ethereal.email>',
+    to,
+    subject,
+    text,
+  });
+
+  console.log(
+    "Email sent! Preview : %s",
+    nodemailer.getTestMessageUrl(info)
+  );
+}
+
+module.exports = sendEmail;
